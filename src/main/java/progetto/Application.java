79137c0975ca1f1e6b1e59ceefadb8fbca58aab3
VittoriaBasile <vichy.basile@gmail.com>
2023-05-19 17:54:58 +0200

    last commit

--- src/main/java/progetto/Application.java
+++ src/main/java/progetto/Application.java
@@ -1,6 +1,8 @@
 package progetto;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -43,56 +45,71 @@ public class Application {
 		Prestito prestito4 = new Prestito(utente4, rivista1, LocalDate.of(2023, 5, 2));
 		Prestito prestito5 = new Prestito(utente4, rivista2, LocalDate.of(2023, 4, 2));
 
-		/*
-		 * cd.save(libro1); cd.save(libro2); cd.save(rivista1); cd.save(rivista2);
-		 * 
-		 * ud.save(utente1); ud.save(utente2); ud.save(utente3); ud.save(utente4);
-		 * 
-		 * pd.save(prestito1); pd.save(prestito2); pd.save(prestito3);
-		 * pd.save(prestito4); pd.save(prestito5);
-		 * 
-		 * cd.delete(UUID.fromString("216d34a7-7a44-4c01-b205-66f9de3a7688"));
-		 * 
-		 * log.info(
-		 * "***************************************TROVATO PER ANNO************************************************"
-		 * ); List<Catalogo> trovatoPerAnno = cd.getByYear(2022); if
-		 * (trovatoPerAnno.size() > 0) { trovatoPerAnno.stream().forEach(c ->
-		 * log.info(c.toString())); } else {
-		 * log.info("NESSUN CATALOGO TROVATO SCRITTO NELL' ANNO INDICATO"); }
-		 * 
-		 * log.info(
-		 * "***************************************TROVATO PER TITOLO************************************************"
-		 * ); List<Catalogo> trovatoPerTitolo = cd.getByTitle("Harry"); if
-		 * (trovatoPerTitolo.size() > 0) { trovatoPerTitolo.stream().forEach(c ->
-		 * log.info(c.toString())); } else {
-		 * log.info("NESSUN CATALOGO TROVATO AVENTE IL TITOLO INDICATO"); }
-		 * 
-		 * log.info(
-		 * "**************************************TROVATO PER AUTORE***************************************************"
-		 * ); List<Libro> trovatoPerAutore = cd.getByAuthor("J.K.Rowling"); if
-		 * (trovatoPerAutore.size() > 0) { trovatoPerAutore.stream().forEach(l ->
-		 * log.info(l.toString())); } else {
-		 * log.info("NESSUN LIBRO TROVATO AVENTE L' AUTORE INDICATO"); }
-		 * 
-		 * log.info(
-		 * "**************************************PRESTITO ATTIVO TROVATO PER NUMERO DI TESSERA***************************************************"
-		 * ); List<Prestito> trovatoPerNumeroDiTessera = pd
-		 * .getByNumeroDiTessera(UUID.fromString("c944ecba-67f7-4b68-836b-5063e77a8c8f")
-		 * ); if (trovatoPerNumeroDiTessera.size() > 0) {
-		 * trovatoPerNumeroDiTessera.stream().forEach(l -> log.info(l.toString())); }
-		 * else { log.
-		 * info("NESSUN PRESTITO TROVATO PER L' UTENTE AVENTE IL NUMERO DI TESSERA INSERITO"
-		 * ); }
-		 * 
-		 * log.info(
-		 * "**************************************PRESTITO SCADUTO TROVATO***************************************************"
-		 * ); List<Prestito> trovatoPrestitiScaduti =
-		 * pd.getPrestitiScadutiENonRestituiti();
-		 * 
-		 * if (trovatoPrestitiScaduti.size() > 0) {
-		 * trovatoPrestitiScaduti.stream().forEach(c -> log.info(c.toString())); } else
-		 * { log.info("NESSUN PRESTITO SCADUTO O NON RESTITUITO TROVATO"); }
-		 */
+		cd.save(libro1);
+		cd.save(libro2);
+		cd.save(rivista1);
+		cd.save(rivista2);
+
+		ud.save(utente1);
+		ud.save(utente2);
+		ud.save(utente3);
+		ud.save(utente4);
+
+		pd.save(prestito1);
+		pd.save(prestito2);
+		pd.save(prestito3);
+		pd.save(prestito4);
+		pd.save(prestito5);
+
+		cd.delete(UUID.fromString("216d34a7-7a44-4c01-b205-66f9de3a7688"));
+
+		log.info(
+				"***************************************TROVATO PER ANNO************************************************");
+		List<Catalogo> trovatoPerAnno = cd.getByYear(2022);
+		if (trovatoPerAnno.size() > 0) {
+			trovatoPerAnno.stream().forEach(c -> log.info(c.toString()));
+		} else {
+			log.info("NESSUN CATALOGO TROVATO SCRITTO NELL' ANNO INDICATO");
+		}
+
+		log.info(
+				"***************************************TROVATO PER TITOLO************************************************");
+		List<Catalogo> trovatoPerTitolo = cd.getByTitle("Harry");
+		if (trovatoPerTitolo.size() > 0) {
+			trovatoPerTitolo.stream().forEach(c -> log.info(c.toString()));
+		} else {
+			log.info("NESSUN CATALOGO TROVATO AVENTE IL TITOLO INDICATO");
+		}
+
+		log.info(
+				"**************************************TROVATO PER AUTORE***************************************************");
+		List<Libro> trovatoPerAutore = cd.getByAuthor("J.K.Rowling");
+		if (trovatoPerAutore.size() > 0) {
+			trovatoPerAutore.stream().forEach(l -> log.info(l.toString()));
+		} else {
+			log.info("NESSUN LIBRO TROVATO AVENTE L' AUTORE INDICATO");
+		}
+
+		log.info(
+				"**************************************PRESTITO ATTIVO TROVATO PER NUMERO DI TESSERA***************************************************");
+		List<Prestito> trovatoPerNumeroDiTessera = pd
+				.getByNumeroDiTessera(UUID.fromString("c944ecba-67f7-4b68-836b-5063e77a8c8f"));
+		if (trovatoPerNumeroDiTessera.size() > 0) {
+			trovatoPerNumeroDiTessera.stream().forEach(l -> log.info(l.toString()));
+		} else {
+			log.info("NESSUN PRESTITO TROVATO PER L' UTENTE AVENTE IL NUMERO DI TESSERA INSERITO");
+		}
+
+		log.info(
+				"**************************************PRESTITO SCADUTO TROVATO***************************************************");
+		List<Prestito> trovatoPrestitiScaduti = pd.getPrestitiScadutiENonRestituiti();
+
+		if (trovatoPrestitiScaduti.size() > 0) {
+			trovatoPrestitiScaduti.stream().forEach(c -> log.info(c.toString()));
+		} else {
+			log.info("NESSUN PRESTITO SCADUTO O NON RESTITUITO TROVATO");
+		}
+
 		em.close();
 		emf.close();
 
